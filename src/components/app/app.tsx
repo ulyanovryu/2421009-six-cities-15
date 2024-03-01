8ba5de9d7ae2ca8cad29c481631f50ef2fcc35b9
ulyanovryu <ulyanovryu@iek.ru>
2024-03-02 01:38:35 +0300

    3.16.1 Настроен роутер.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,15 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import {AppRoute, AuthorizationStatus} from '../../const';
+
+import Layout from '../../components/layout/layout.tsx';
+import PrivateRoute from '../../components/private-route/private-route.tsx';
+
 import MainScreen from '../../pages/main-screen/main-screen.tsx';
+import LoginScreen from '../../pages/login-screen/login-screen.tsx';
+import OfferScreen from '../../pages/offer-screen/offer-screen.tsx';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen.tsx';
+import Page404 from '../../pages/page404-screen/page404-screen.tsx';
 
 type AppProps = {
   offersCount: number;
@@ -6,7 +17,22 @@ type AppProps = {
 
 function App({offersCount}: AppProps): JSX.Element {
   return (
-    <MainScreen offersCount={offersCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<Layout authorizationStatus={AuthorizationStatus.NoAuth} />}>
+          <Route index element={<MainScreen offersCount={offersCount}/>} />
+          <Route path={AppRoute.Login} element={<LoginScreen />} />
+          <Route path={AppRoute.Favorites} element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+          />
+          <Route path={AppRoute.Offer} element={<OfferScreen />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
