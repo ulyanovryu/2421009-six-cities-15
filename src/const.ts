c600cb39bccfca61153dec401fdf01f32839c09c
ulyanovryu <ulyanovryu@iek.ru>
2024-03-02 01:33:35 +0300

    3.16.1 Добавлены константы

---
+++ src/const.ts
@@ -0,0 +1,13 @@
+
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
