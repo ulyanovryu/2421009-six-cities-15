e256fd544b9e3e68ebaf881449963a5ed3f0b481
ulyanovryu <ulyanovryu@iek.ru>
2024-03-09 14:49:34 +0300

    4.21.1. Добавил типы и моки рейтинга

---
+++ src/mocks/rating.ts
@@ -0,0 +1,20 @@
+import {Ratings} from '../types/rating.ts';
+
+export const ratingsList: Ratings = [
+  {
+    'title': 'terribly',
+    'value' : 1,
+  },{
+    'title': 'badly',
+    'value' : 2,
+  },{
+    'title': 'not bad',
+    'value' : 3,
+  },{
+    'title': 'good',
+    'value' : 4,
+  },{
+    'title': 'perfect',
+    'value' : 5,
+  },
+];
