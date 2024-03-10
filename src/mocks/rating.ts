c48991614aed816eac7ca531f371e502458d73ba
ulyanovryu <ulyanovryu@iek.ru>
2024-03-11 00:46:29 +0300

    4.21.13. Изменен порядок.

--- src/mocks/rating.ts
+++ src/mocks/rating.ts
@@ -2,19 +2,19 @@ import {Ratings} from '../types/rating.ts';
 
 export const ratingsList: Ratings = [
   {
-    'title': 'terribly',
-    'value' : 1,
-  },{
-    'title': 'badly',
-    'value' : 2,
-  },{
-    'title': 'not bad',
-    'value' : 3,
+    'title': 'perfect',
+    'value' : 5,
   },{
     'title': 'good',
     'value' : 4,
   },{
-    'title': 'perfect',
-    'value' : 5,
+    'title': 'not bad',
+    'value' : 3,
+  },{
+    'title': 'badly',
+    'value' : 2,
+  },{
+    'title': 'terribly',
+    'value' : 1,
   },
 ];
