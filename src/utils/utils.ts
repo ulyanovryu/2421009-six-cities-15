147e226dbc2f681e10f057dce7c2a425aeb8a384
ulyanovryu <ulyanovryu@iek.ru>
2024-03-11 00:52:33 +0300

    4.21.14. Добавлена общая функция

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
 import {AuthorizationStatus} from '../const.ts';
 const getAuthorizationStatus = () => AuthorizationStatus.Auth;
 export default getAuthorizationStatus;
+export const upperString = (stringToUpFirstLetter: string): string => stringToUpFirstLetter.charAt(0).toUpperCase() + stringToUpFirstLetter.slice(1);
+
