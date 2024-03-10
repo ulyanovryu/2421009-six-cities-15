eaae3de483375d41fde1ea17f5de3bd619c8d010
ulyanovryu <ulyanovryu@iek.ru>
2024-03-10 16:13:31 +0300

    4.21.5. Добавил вывод списка отзывов

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,37 @@
+import {Reviews} from '../types/reviews.ts';
+
+const ReviewsListData: Reviews = [
+  {
+    'id': '1af6f711-c28d-4121-82cd-e0b462a27f00',
+    'date': '2021-05-08T14:13:56.569Z',
+    'user': {
+      'name': 'Oliver Con 1',
+      'avatarUrl': 'img/avatar-max.jpg',
+      'isPro': false
+    },
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'rating': 4
+  },{
+    'id': '1af6f711-c28d-4121-82cd-e0b462a27f00',
+    'date': '2020-05-08T14:13:56.569Z',
+    'user': {
+      'name': 'Oliver Coer 2',
+      'avatarUrl': 'img/avatar-max.jpg',
+      'isPro': true
+    },
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'rating': 3
+  },{
+    'id': '1af6f711-c28d-4121-82cd-e0b462a27f00',
+    'date': '2023-05-08T14:13:56.569Z',
+    'user': {
+      'name': 'Oliver Cer 3',
+      'avatarUrl': 'img/avatar-max.jpg',
+      'isPro': false
+    },
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'rating': 5
+  },
+];
+
+export default ReviewsListData;
---
+++ src/types/reviews.ts
@@ -0,0 +1,13 @@
+export type Review = {
+  id: string;
+  date: string;
+  user: {
+    name: string;
+    avatarUrl: string;
+    isPro: boolean;
+  };
+  comment: string;
+  rating: number;
+};
+
+export type Reviews = Review[];
