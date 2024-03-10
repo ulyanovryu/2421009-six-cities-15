452cd68e4a441f8996bbbfec0c5634f266ce54cd
ulyanovryu <ulyanovryu@iek.ru>
2024-03-11 00:53:10 +0300

    4.21.15. Добавлена города и предложения из моков.

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app.tsx';
 import Settings from './components/settings/settings.tsx';
 
+import citiesList from './mocks/cities.ts';
+import offersList from './mocks/offers.ts';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App offersCount={Settings.OffersCount}/>
+    <App offersCount={Settings.OffersCount} offersList={offersList} citiesList={citiesList} />
   </React.StrictMode>
 );
