cf83ce4e411f84f954742da2e52a5d31b3497c77
ulyanovryu <ulyanovryu@iek.ru>
2024-03-22 00:52:30 +0300

    6.15.1.3 Удалил action в соответствии с рекомендациями.

--- src/store/action.ts
+++
@@ -1,6 +0,0 @@
-import {createAction} from '@reduxjs/toolkit';
-import {City} from '../types/cities.ts';
-import {Offers} from '../types/offers.ts';
-
-export const changeCity = createAction<{city: City}>('city/change');
-export const changeOffers = createAction<{offers: Offers}>('offers/change');
