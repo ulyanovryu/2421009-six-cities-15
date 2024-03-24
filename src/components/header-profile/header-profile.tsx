12878596783aefc65e5dea08ee20ab9cc5f56ae0
ulyanovryu <ulyanovryu@iek.ru>
2024-03-24 21:58:49 +0300

    7.15.4. Настроил отображение почты и аватара авторизованного пользователя; настроил выход по кнопке "выход"

--- src/components/header-profile/header-profile.tsx
+++ src/components/header-profile/header-profile.tsx
@@ -1,19 +1,20 @@
 import {AppRoute, AuthorizationStatus} from '../../const';
 import {Link} from 'react-router-dom';
-import getAuthorizationStatus from '../../utils/utils.ts';
+import {useAppSelector} from '../../hooks';
+import {authorizationSelectors} from '../../store/slices/auth.ts';
 
 function HeaderProfile(): JSX.Element {
 
-  const authorizationStatus = getAuthorizationStatus();
+  const authorizationStatus = useAppSelector(authorizationSelectors.authorizationStatus);
+  const {email, avatarUrl} = useAppSelector(authorizationSelectors.user);
 
   return (
     authorizationStatus === AuthorizationStatus.Auth
       ?
       <li className="header__nav-item user">
         <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile">
-          <div className="header__avatar-wrapper user__avatar-wrapper">
-          </div>
-          <span className="header__user-name user__name">O1liver.conner@gmail.com</span>
+          <div className="header__avatar-wrapper user__avatar-wrapper" style={{backgroundImage:`url(${avatarUrl})`}}></div>
+          <span className="header__user-name user__name">{email}</span>
           <span className="header__favorite-count">3</span>
         </Link>
       </li>
