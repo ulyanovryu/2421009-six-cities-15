e812d08dd619b4a45c643e4714a93724e85ca045
ulyanovryu <ulyanovryu@iek.ru>
2024-03-27 23:22:18 +0300

    7.16.21. Удалены комментарии.

--- src/types/offers.ts
+++ src/types/offers.ts
@@ -22,41 +22,6 @@ export type OfferList = {
   previewImage: string;
 };
 
-/*export type Offer = {
-  id: string;
-  title: string;
-  type: string;
-  price: number;
-  city: {
-    name: string;
-    location: {
-      latitude: number;
-      longitude: number;
-      zoom: number;
-    };
-  };
-  location: {
-    latitude: number;
-    longitude: number;
-    zoom: number;
-  };
-  isFavorite: boolean;
-  isPremium: boolean;
-  rating: number;
-  previewImage: string;
-  description?: string;
-  bedrooms?: number;
-  goods?: string[];
-  host?: {
-    name: string;
-    avatarUrl: string;
-    isPro: boolean;
-  };
-  images?: string[];
-  maxAdults?: number;
-};*/
-
-
 export type OfferDetail = OfferList & {
   description: string;
   bedrooms: number;
