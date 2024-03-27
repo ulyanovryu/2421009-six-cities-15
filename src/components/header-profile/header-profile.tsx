66ae025f7fc1911e706e54e2087135051de58542
ulyanovryu <ulyanovryu@iek.ru>
2024-03-27 17:02:28 +0300

    7.16.17. Добавлен хук подсчета и перезапроса избранного.

--- src/components/header-profile/header-profile.tsx
+++ src/components/header-profile/header-profile.tsx
@@ -1,12 +1,14 @@
 import {AppRoute} from '../../const';
 import {Link} from 'react-router-dom';
 import {useAppSelector} from '../../hooks';
+import {useFavoriteCount} from '../../hooks/use-favorite-count.ts';
 import {userSelectors} from '../../store/slices/user.ts';
 import {useAuth} from '../../hooks/user-authorization.ts';
 
 function HeaderProfile(): JSX.Element {
 
   const {email, avatarUrl} = useAppSelector(userSelectors.user);
+  const count = useFavoriteCount();
 
   return (
     useAuth()
@@ -15,7 +17,7 @@ function HeaderProfile(): JSX.Element {
         <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile">
           <div className="header__avatar-wrapper user__avatar-wrapper" style={{backgroundImage:`url(${avatarUrl})`}}></div>
           <span className="header__user-name user__name">{email}</span>
-          <span className="header__favorite-count">3</span>
+          <span className="header__favorite-count">{count}</span>
         </Link>
       </li>
       :
