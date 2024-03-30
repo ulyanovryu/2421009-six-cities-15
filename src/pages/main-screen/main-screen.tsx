45bd7336ebe2c926b4f1b42295665e9ad07ff694
ulyanovryu <ulyanovryu@iek.ru>
2024-03-30 16:20:39 +0300

    8.13.2 добавлена город на странице-заглушке в случае отсутствия предложений

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -81,7 +81,7 @@ function MainScreen ({city}: MainScreenProps): JSX.Element {
                 <section className="cities__no-places">
                   <div className="cities__status-wrapper tabs__content">
                     <b className="cities__status">No places to stay available</b>
-                    <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+                    <p className="cities__status-description">We could not find any property available at the moment in {city}</p>
                   </div>
                 </section>
                 <div className="cities__right-section"></div>
